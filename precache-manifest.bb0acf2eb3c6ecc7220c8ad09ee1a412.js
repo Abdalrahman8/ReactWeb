918d9a3688ca1156db61efc499fadee9bcac6704
Abdalrahman8 <a_saleh@azhar.edu.eg>
2021-03-09 06:43:57 +0100

    Updates

---
+++ precache-manifest.bb0acf2eb3c6ecc7220c8ad09ee1a412.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcd4fc3c9113aa0f64b714d73f438cc9",
+    "url": "/ReactWeb/index.html"
+  },
+  {
+    "revision": "6d17dd144d8f0c1f2483",
+    "url": "/ReactWeb/static/css/main.996b54b1.chunk.css"
+  },
+  {
+    "revision": "d271e05f5759841e4a19",
+    "url": "/ReactWeb/static/js/2.ea88539f.chunk.js"
+  },
+  {
+    "revision": "6d17dd144d8f0c1f2483",
+    "url": "/ReactWeb/static/js/main.ea2f2384.chunk.js"
+  },
+  {
+    "revision": "e1c93565d87488de4c4e",
+    "url": "/ReactWeb/static/js/runtime~main.e6eeee68.js"
+  }
+]);
